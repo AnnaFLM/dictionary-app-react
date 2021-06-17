4e622945797d9506ff22d968ce35865ed5c67283
Anna Mehnert <mehnert.anna@gmail.com>
2021-06-17 23:00:22 +0200

    created Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a href="#" className="btn btn-primary">
-          Hello
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <a href="#" className="btn btn-primary">
+            Hello hello
+          </a>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>coded by Anna</small>
+        </footer>
+      </div>
     </div>
   );
 }
