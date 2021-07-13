a621d48f0f185fd2ce7b8072a46f634b24453b02
Anna Mehnert <mehnert.anna@gmail.com>
2021-07-13 21:24:49 +0200

    Update Synonyms.js

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,6 +5,9 @@ export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <ul className="Synonyms">
+        <div>
+          <strong>similar:</strong>
+        </div>
         {props.synonyms.map(function (synonym, index) {
           return <li key={index}>{synonym}</li>;
         })}
